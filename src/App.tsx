d58d18f260252f302bf15cf19bc6511ddd4c58e8
flamingotiger <hhbhong@gmail.com>
2020-01-07 21:56:04 +0900

    Add main style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Timer from './Timer';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faCheck, faPause } from '@fortawesome/free-solid-svg-icons'
+
+const TimerWrapper = styled.div`
+    width: 440px;
+    height: 520px;
+    background-color: black;
+    border-radius: 60px;
+    padding: 30px;
+    box-sizing: border-box;
+`
+
+const TimerDisplay = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`
+const TimerHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 60px;
+`
+const BackButton = styled.button`
+    background: none;
+    border: none;
+    color: rgb(223,117,57);
+    font-size: 30px;
+    line-height: 60px;
+    font-weight: 300;
+    cursor: pointer;
+`
+
+const BackText = styled.span`
+    font-size: 36px;
+    margin-left: 10px;
+`
+
+const CurrentTime = styled.span`
+    font-size: 36px;
+    line-height: 60px;
+    color: rgb(108,108,108);
+    margin-right: 10px;
+`
+
+const ButtonsWrapper = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 60px;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const Button = styled.button`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    border: 0;
+    background-color: rgb(38,38,38);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TimerWrapper>
+      <TimerDisplay>
+        <TimerHeader>
+          <BackButton>
+            <FontAwesomeIcon icon={faChevronLeft} color="rgb(106,106,106)" size="sm"/> 
+            <BackText>Back</BackText>
+          </BackButton>
+          <CurrentTime>10:00</CurrentTime>
+        </TimerHeader>
+        <Timer />
+        <ButtonsWrapper>
+          <Button type="button">
+            <FontAwesomeIcon icon={faPause} color="#fff" size="lg"/> 
+          </Button>
+          <Button type="button">
+            <FontAwesomeIcon icon={faCheck} color="#fff" size="lg"/> 
+          </Button>
+        </ButtonsWrapper>
+      </TimerDisplay>
+    </TimerWrapper>
   );
 }
 
---
+++ src/Timer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Timer:React.FC = () => {
+    return <div>
+        <p>2:35</p>
+        <p>Timer</p>
+    </div>
+}
+export default Timer;
