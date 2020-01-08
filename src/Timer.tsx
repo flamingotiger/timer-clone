d9072c95b9bac5b55f6d0e70262792cc78d5d704
flamingotiger <hhbhong@gmail.com>
2020-01-08 09:54:00 +0900

    Fix timer draw warning

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const TimerStyle = styled.div`
@@ -19,36 +19,35 @@ const Timer: React.FC = () => {
         return degrees / 180 * Math.PI;
     };
 
-    const drawPath = (angle: number) => {
-        const getArc = (angle: number) => {
-            const x = center + radius * Math.cos(radians(angle));
-            const y = center + radius * Math.sin(radians(angle));
-            return `A${radius},${radius} 1 0 1 ${x},${y}`; // 곡선
-        };
-        const firstAngle = angle > 180 ? 90 : angle - 90; // 왼쪽 각도
-        const secondAngle = -270 + angle - 180; // 오른쪽 각도
-        const firstArc = getArc(firstAngle); // 왼쪽 반원
-        const secondArc = angle > 180 ? getArc(secondAngle) : ''; // 오른쪽 반원
-        const start = `M${center},${center} m0,-${center - strokeWidth / 2}`; // 시작점
-        const d = `${start} ${firstArc} ${secondArc}`; // 시작점 왼쪽 반원 오른쪽 반원 그리는 값
-        setDraw(d);
-        setAngle(angle);
-    }
-
     useEffect(() => {
+        const drawPath = (angle: number) => {
+            const getArc = (angle: number) => {
+                const x = center + radius * Math.cos(radians(angle));
+                const y = center + radius * Math.sin(radians(angle));
+                return `A${radius},${radius} 1 0 1 ${x},${y}`; // 곡선
+            };
+            const firstAngle = angle > 180 ? 90 : angle - 90; // 왼쪽 각도
+            const secondAngle = -270 + angle - 180; // 오른쪽 각도
+            const firstArc = getArc(firstAngle); // 왼쪽 반원
+            const secondArc = angle > 180 ? getArc(secondAngle) : ''; // 오른쪽 반원
+            const start = `M${center},${center} m0,-${center - strokeWidth / 2}`; // 시작점
+            const d = `${start} ${firstArc} ${secondArc}`; // 시작점 왼쪽 반원 오른쪽 반원 그리는 값
+            setDraw(d);
+            setAngle(angle);
+        }
         drawPath(angle);
-    }, [angle]);
+    }, [angle, center, radius]);
 
     return <TimerStyle>
-        <SvgWrapper viewBox={`0 0 ${size} ${size}`} >
+        <SvgWrapper viewBox={`0 0 ${size} ${size}`}>
             <defs>
                 <linearGradient id="gradient">
-                    <stop offset="0%" stopColor="rgb(245,221,100)" />
-                    <stop offset="100%" stopColor="rgb(234,111,37)" />
+                    <stop offset="0%" stopColor="rgb(245,221,100)"/>
+                    <stop offset="100%" stopColor="rgb(234,111,37)"/>
                 </linearGradient>
             </defs>
-            <circle cx={center} cy={center} r={center} fill='rgb(20,13,4)' stroke='none' />
-            <circle cx={center} cy={center} r={center - strokeWidth} fill='#000' stroke='none' />
+            <circle cx={center} cy={center} r={center} fill='rgb(20,13,4)' stroke='none'/>
+            <circle cx={center} cy={center} r={center - strokeWidth} fill='#000' stroke='none'/>
             <path
                 strokeLinecap="round"
                 strokeWidth={strokeWidth}
@@ -56,8 +55,12 @@ const Timer: React.FC = () => {
                 fill='none'
                 d={draw}
             />
-            <text x={center - 25} y={center + 2} fill="#fff" fontSize="20" fontWeight="900">02:35</text>
-            <text x={center - 20} y={center + 15} fill="rgb(108,108,108)" fontSize="8">Watch Timer</text>
+            <text x={center - 25} y={center + 2} fill="#fff" fontSize="20" fontWeight="900"
+                  style={{userSelect: 'none'}}>02:35
+            </text>
+            <text x={center - 20} y={center + 15} fill="rgb(108,108,108)" fontSize="8"
+                  style={{userSelect: 'none'}}>Watch Timer
+            </text>
         </SvgWrapper>
     </TimerStyle>
 }
