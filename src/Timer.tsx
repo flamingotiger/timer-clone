50fbabad5023cfb7014f92bc0b74e38275f30712
flamingotiger <hhbhong@gmail.com>
2020-01-08 15:14:35 +0900

    Finished

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -10,13 +10,13 @@ const SvgWrapper = styled.svg`
 `
 
 const Timer: React.FC<{ timer: Duration, defaultTime: number }> = ({timer, defaultTime}) => {
+    const [angle, setAngle] = useState(360); // 애니메이션 시작 이전 각도
+    const [prevAngle, setPrevAngle] = useState(angle); // 애니메이션 시작 이전 각도
     const size = 100;
     const center = size / 2;
     const strokeWidth = 10;
     const radius = center - strokeWidth / 2;
-    const [angle, setAngle] = useState(360); // 기본각도
     const animDuration = 300; // 애니메이션 속도
-    const [prevAngle, setPrevAngle] = useState(angle); // 애니메이션 시작 이전 각도
     const [draw, setDraw] = useState(``); // path d 값
 
     const radians = (degrees: number) => {
@@ -26,13 +26,13 @@ const Timer: React.FC<{ timer: Duration, defaultTime: number }> = ({timer, defau
     React.useEffect(() => {
         if (timer.asMilliseconds() >= 0) {
             const percent = timer.asMilliseconds() / defaultTime;
-            const angle = 360 * percent;
-            setPrevAngle(angle);
+            const prevAngle = 360 * percent;
+            setPrevAngle(prevAngle);
         }
     }, [timer, defaultTime])
 
     useEffect(() => {
-        const drawPath = (angle: number) => {
+        const drawPath = (angle: number, finished: boolean) => {
             const getArc = (angle: number) => {
                 const x = center + radius * Math.cos(radians(angle));
                 const y = center + radius * Math.sin(radians(angle));
@@ -45,17 +45,19 @@ const Timer: React.FC<{ timer: Duration, defaultTime: number }> = ({timer, defau
             const start = `M${center},${center} m0,-${center - strokeWidth / 2}`; // 시작점
             const d = `${start} ${firstArc} ${secondArc}`; // 시작점 왼쪽 반원 오른쪽 반원 그리는 값
             setDraw(d);
-            setAngle(angle);
+            if (finished) {
+                setAngle(angle);
+            }
         }
 
         const step = (angleOffset: number, argAngle: number, time: number, endTime: number) => {
             const now = new Date().valueOf(); // 애니메이션 시작 시간
             const timeOffset = endTime - now; // 끝나는 시간 - 시작 시간
             if (timeOffset <= 0) { // 끝나는 시간이 지났을때
-                drawPath(argAngle);
+                drawPath(argAngle, true);
             } else {
                 const incrementAngle = argAngle - (angleOffset * timeOffset / time); // 이전각도 - (이전각도 - 현재각도) * (끝나는 시간 - 시작 시간) / 애니메이션 시간
-                drawPath(incrementAngle); // path를 그려줍니다.
+                drawPath(incrementAngle, false); // path를 그려줍니다.
                 requestAnimationFrame(() => step(angleOffset, argAngle, time, endTime)); // 끝나는 시간이 지날때까지 step을 계속 실행시킵니다.
             }
         }
@@ -70,7 +72,7 @@ const Timer: React.FC<{ timer: Duration, defaultTime: number }> = ({timer, defau
             requestAnimationFrame(() => step(angleOffset, argAngle, time, endTime));
         }
         anim(prevAngle, animDuration);
-    }, [prevAngle, animDuration]);
+    }, [prevAngle, animDuration, center, radius, angle]);
 
     return <TimerStyle>
         <SvgWrapper viewBox={`0 0 ${size} ${size}`}>
